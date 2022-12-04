d67d7972b1bf2113fbba1ea46cd75ff17569ca68
Fredrik Mile <fredrik.mile@gmail.com>
2022-12-04 08:16:26 +0100

    update template

--- template.hs
+++ template.hs
@@ -1,9 +1,5 @@
-myHead :: [String] -> String
-myHead ss = head ss
-
 main = do
         contents <- readFile "input.txt"
         let rows = lines contents
-        let first = myHead(rows)
-        print(first)
+        print(rows)
 
