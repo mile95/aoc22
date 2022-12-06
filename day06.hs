2cd42fc24056bb1056d0b55bfd6d4fa37d1fce54
Fredrik Mile <fredrik.mile@gmail.com>
2022-12-06 07:05:27 +0100

    add day06.hs

---
+++ day06.hs
@@ -0,0 +1,17 @@
+findMarker :: String -> Int -> Int -> Int -> Int 
+findMarker s l start end = 
+  if allDifferent(subList) && length subList == l then end 
+  else findMarker s l (start+1) (end+1)
+    where subList = drop start . take end $ s
+
+allDifferent :: String -> Bool
+allDifferent [] = True
+allDifferent (x:xs) = x `notElem` xs && allDifferent xs
+
+main = do
+        contents <- readFile "input.txt"
+        let rows = lines contents
+        let packetStart = findMarker (head rows) 4 0 4
+        let messageStart = findMarker (head rows) 14 0 14
+        print(packetStart)
+        print(messageStart)
