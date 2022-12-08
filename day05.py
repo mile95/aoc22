65cfbec195af681f3c953ad41781727c854e4167
Fredrik Mile <fredrik.mile@gmail.com>
2022-12-08 21:20:00 +0100

    add day05.py

---
+++ day05.py
@@ -0,0 +1,34 @@
+with open("input.txt") as f:
+    input = f.read().splitlines()
+
+
+moves = [x for x in input if "move" in x]
+setup = [x for x in input if "[" in x]
+
+setup = ([*zip(*setup)])
+
+init = []
+for s in setup:
+    filtered = ''.join(filter(str.isalpha, s))
+    if filtered:
+         init.append(filtered[::-1])
+
+for move in moves:
+    q = int(move.split()[1])
+    f = int(move.split()[3])
+    t = int(move.split()[5])
+
+    to_move = init[f-1][-q:]
+
+    # for part A, iteratate the reversed list. to_move[::-1]
+    for m in to_move:
+         init[t-1] += m
+
+    init[f-1] = init[f-1][:-q]
+
+word = ""
+
+for i in init:
+    word += i[-1]
+
+print(word)
