6068966d09512542e83c3b646bfec355e462c64b
Fredrik Mile <fredrik.mile@gmail.com>
2022-12-11 08:57:12 +0100

    fmt

--- day05.py
+++ day05.py
@@ -5,26 +5,26 @@ with open("input.txt") as f:
 moves = [x for x in input if "move" in x]
 setup = [x for x in input if "[" in x]
 
-setup = ([*zip(*setup)])
+setup = [*zip(*setup)]
 
 init = []
 for s in setup:
-    filtered = ''.join(filter(str.isalpha, s))
+    filtered = "".join(filter(str.isalpha, s))
     if filtered:
-         init.append(filtered[::-1])
+        init.append(filtered[::-1])
 
 for move in moves:
     q = int(move.split()[1])
     f = int(move.split()[3])
     t = int(move.split()[5])
 
-    to_move = init[f-1][-q:]
+    to_move = init[f - 1][-q:]
 
     # for part A, iteratate the reversed list. to_move[::-1]
     for m in to_move:
-         init[t-1] += m
+        init[t - 1] += m
 
-    init[f-1] = init[f-1][:-q]
+    init[f - 1] = init[f - 1][:-q]
 
 word = ""
 
--- day09.py
+++ day09.py
@@ -6,35 +6,37 @@ with open("input.txt") as f:
         moves.append((d, int(q)))
 
 
-
 dir = {
-    'R': (1, 0),
-    'L': (-1, 0),
-    'U': (0, 1),
-    'D': (0, -1),
+    "R": (1, 0),
+    "L": (-1, 0),
+    "U": (0, 1),
+    "D": (0, -1),
 }
 
+
 def follow(h, t):
-    x, y = h[0] - t[0], h[1] - t[1] 
+    x, y = h[0] - t[0], h[1] - t[1]
     if abs(x) > 1 or abs(y) > 1:
         return (
             t[0] + (0 if x == 0 else x // abs(x)),
-            t[1] + (0 if y == 0 else y // abs(y))
+            t[1] + (0 if y == 0 else y // abs(y)),
         )
     return t
 
-def solve(knots): 
+
+def solve(knots):
     ropes = [(0, 0)] * knots
-    visited = {ropes[-1]} 
+    visited = {ropes[-1]}
     for move in moves:
         change = dir[move[0]]
         for _ in range(move[1]):
             head = ropes[0]
             ropes[0] = head[0] + change[0], head[1] + change[1]
             for x in range(1, knots):
-                ropes[x] = follow(ropes[x-1], ropes[x])
+                ropes[x] = follow(ropes[x - 1], ropes[x])
             visited.add(ropes[-1])
     return len(visited)
+
 
 print(solve(2))
 print(solve(10))
