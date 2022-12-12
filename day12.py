7277bc19259d2bb1f9ba940e65567319efe15448
Fredrik Mile <fredrik.mile@gmail.com>
2022-12-12 21:29:39 +0100

    add solution for part a day12

--- day12.py
+++ day12.py
@@ -6,37 +6,91 @@ with open("input.txt") as f:
     input = f.read().splitlines()
 
 
-def find_start():
+def find_index(item):
     for i, r in enumerate(input):
-        if 'S' in r:
-            return (i, r.index('S'))
+        if item in r:
+            return (i, r.index(item))
 
-def solve(pos, path):
-    x, y = pos
-    val = input[x][y]
-    candidates = []
-    for move in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-        xc, yc = move
-        try:
-            cand = input[x + xc][y + yc]
-            current_height = 0 if val == "S" else alphabet.index(val)
-            if (x + xc, y + yc) not in path:
-                cand_height = alphabet.index('z') if cand == "E" else alphabet.index(cand)
-                if (
-                    (cand_height <= (current_height + 1))
-                    and (x + xc >= 0 and y + yc >= 0)
-                ):
-                    candidates.append((x + xc, y + yc))
-        except IndexError:
-            pass
-    for c in candidates:
-        if input[c[0]][c[1]] == 'E':
-            print("Woho! Found E!")
-            print(len(path))
-            break
+
+def dijkstra(graph, start_pos, target):
+    distances = {}
+    prev = {}
+    Q = set()
+
+    for n in graph:
+        distances[n] = 100000000
+        prev[n] = None
+        Q.add(n)
+
+    distances[start_pos] = 0
+
+    while len(Q) > 0:
+        min_dist = 100000000
+        u = None
+
+        for n in Q:
+            if distances[n] < min_dist:
+                min_dist = distances[n]
+                u = n
+
+        if u is None:
+            u = Q.pop()
         else:
-            path.append(pos)
-            solve(c, path)
+            Q.remove(u)
 
-start_pos = find_start()
-solve(start_pos, [])
+        xu, yu = u
+        val = input[xu][yu]
+
+        if u == target:
+            return distances, prev
+
+        candidates = []
+        for move in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            candidates.append((xu + move[0], yu + move[1]))
+
+        # Compute cost as the height.
+        for c in candidates:
+            alt = distances[u]
+            try:
+                c_val = input[c[0]][c[1]]
+
+                if c_val == "E":
+                    c_h = alphabet.index("z")
+                elif c_val == "S":
+                    c_h = 0
+                else:
+                    c_h = alphabet.index(c_val)
+
+                if val not in ["E", "S"]:
+                    if c_h > alphabet.index(val) + 1:
+                        alt = 100000000
+                    else:
+                        alt += 1
+                if alt < distances[c]:
+                    distances[c] = alt
+                    prev[c] = u
+            # Lazy way of handling boundaries in the grid!
+            except Exception:
+                pass
+
+    return distances, prev
+
+
+start_pos = find_index("S")
+end_pos = find_index("E")
+
+graph = []
+for x in range(len(input)):
+    for y in range(len(input[0])):
+        graph.append((x, y))
+
+distances, prev = dijkstra(graph, start_pos, "E")
+
+path = []
+while True:
+    if end_pos == start_pos:
+        break
+    path.append(end_pos)
+    end_pos = prev[end_pos]
+
+print(len(path))
