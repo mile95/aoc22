0a6ba1eafc3eb03a5332506859cef049b23d6a24
Fredrik Mile <fredrik.mile@gmail.com>
2022-12-14 07:56:38 +0100

    add day12 part two

--- day12.py
+++ day12.py
@@ -1,31 +1,27 @@
 import string
+import sys
 
 alphabet = list(string.ascii_lowercase)
 
 with open("input.txt") as f:
     input = f.read().splitlines()
+    input = [[n for n in line] for line in input]
 
 
-def find_index(item):
-    for i, r in enumerate(input):
-        if item in r:
-            return (i, r.index(item))
-
-
-def dijkstra(graph, start_pos, target):
+def dijkstra(graph, start, target):
     distances = {}
     prev = {}
     Q = set()
 
     for n in graph:
-        distances[n] = 100000000
+        distances[n] = sys.maxsize
         prev[n] = None
         Q.add(n)
 
-    distances[start_pos] = 0
+    distances[start] = 0
 
-    while len(Q) > 0:
-        min_dist = 100000000
+    while Q:
+        min_dist = sys.maxsize
         u = None
 
         for n in Q:
@@ -38,59 +34,100 @@ def dijkstra(graph, start_pos, target):
         else:
             Q.remove(u)
 
-        xu, yu = u
-        val = input[xu][yu]
-
         if u == target:
             return distances, prev
+
+        xu, yu = u
 
         candidates = []
         for move in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
             candidates.append((xu + move[0], yu + move[1]))
 
-        # Compute cost as the height.
+        candidates = [c for c in candidates if c in Q]
+
         for c in candidates:
             alt = distances[u]
-            try:
-                c_val = input[c[0]][c[1]]
 
-                if c_val == "E":
-                    c_h = alphabet.index("z")
-                elif c_val == "S":
-                    c_h = 0
+            xc, yc = c
+
+            if input[yc][xc] == "E":
+                current_elevation = alphabet.index("z") + 1
+            elif input[yc][xc] == "S":
+                current_elevation = 0
+            else:
+                current_elevation = alphabet.index(input[yc][xc])
+
+            if input[yu][xu] not in ["S", "E"]:
+                if current_elevation > alphabet.index(input[yu][xu]) + 1:
+                    alt = sys.maxsize
                 else:
-                    c_h = alphabet.index(c_val)
+                    alt += 1
 
-                if val not in ["E", "S"]:
-                    if c_h > alphabet.index(val) + 1:
-                        alt = 100000000
-                    else:
-                        alt += 1
-                if alt < distances[c]:
-                    distances[c] = alt
-                    prev[c] = u
-            # Lazy way of handling boundaries in the grid!
-            except Exception:
-                pass
+            if alt < distances[c]:
+                distances[c] = alt
+                prev[c] = u
 
     return distances, prev
 
 
-start_pos = find_index("S")
-end_pos = find_index("E")
+def get_graph_start_target(data):
+    graph = set()
+    start = None
+    target = None
 
-graph = []
-for x in range(len(input)):
-    for y in range(len(input[0])):
-        graph.append((x, y))
+    for y in range(len(data)):
+        for x in range(len(data[0])):
+            graph.add((x, y))
+            if data[y][x] == "S":
+                start = (x, y)
+            if data[y][x] == "E":
+                target = (x, y)
 
-distances, prev = dijkstra(graph, start_pos, "E")
+    return graph, start, target
+
+
+graph, start, target = get_graph_start_target(input)
+_, prev = dijkstra(graph, start, target)
 
 path = []
+current = target
 while True:
-    if end_pos == start_pos:
+    if current == target:
         break
-    path.append(end_pos)
-    end_pos = prev[end_pos]
+    path.append(current)
+    current = prev[current]
 
 print(len(path))
+
+
+def get_candidates(data):
+    candidates = []
+
+    for y in range(len(data)):
+        for x in range(len(data[0])):
+            if data[y][x] in ["a", "S"]:
+                candidates.append((x, y))
+
+    return candidates
+
+
+start_positions = get_candidates(input)
+start_positions = [(x, y) for (x, y) in start_positions if x == 0]
+
+lengths = []
+for s in start_positions:
+    d, p = dijkstra(graph, s, target)
+
+    path = []
+    current = target
+    last = None
+    while True:
+        if current == s:
+            break
+
+        path.append(current)
+        current = p[current]
+
+    lengths.append(len(path))
+
+print(min(lengths))
